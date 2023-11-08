6dff492cf416892f46d36ed8e8318a4fc1c45593
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-11-08 18:48:36 +0200

    footer

--- src/modules/main/footer/Footer.jsx
+++ src/modules/main/footer/Footer.jsx
@@ -8,15 +8,15 @@ const FooterShop = () => {
   return (
     <Footer className="container">
       <List>
-        <li>
+        <Item>
           <Iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d775665.1977575189!2d-74.41089437890638!3d40.587031695247866!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2z0J3RjNGOLdCZ0L7RgNC6LCDQodC_0L7Qu9GD0YfQtdC90ZYg0KjRgtCw0YLQuCDQkNC80LXRgNC40LrQuA!5e0!3m2!1suk!2sua!4v1699386649348!5m2!1suk!2sua"
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
           ></Iframe>
-        </li>
-        <li>
+        </Item>
+        <Item>
           <WrapperAddress>
             <Title>Our address</Title>
             <OurAddressFooter number="2" />
@@ -25,7 +25,7 @@ const FooterShop = () => {
               <ArrowSvg />
             </BtnGreen>
           </WrapperAddress>
-        </li>
+        </Item>
       </List>
       <Span>{"2020 (c) - Samwyle bandtitos"}</Span>
     </Footer>
@@ -51,6 +51,7 @@ const Iframe = styled.iframe`
   height: 300px;
   filter: grayscale(100%);
   filter: gray;
+  border: 0;
 `;
 
 const WrapperAddress = styled.div`
