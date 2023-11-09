1cbf513b7e9217aee7a060f2152f107805321b0a
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-11-09 22:10:00 +0200

    start work with responsive

--- src/modules/header/Header.jsx
+++ src/modules/header/Header.jsx
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 import MobileHeader from "./mobileHeader/MobileHeader";
+import { useMediaQuery } from "react-responsive";
 
 const Header = () => {
+  // const isBigScreen = useMediaQuery({ query: "(min-width: 1440px)" });
+  const isTabletScreen = useMediaQuery({ query: "(max-width: 768px)" });
   return (
-    <Head className="container">
-      <MobileHeader />
-    </Head>
+    <Head className="container">{isTabletScreen && <MobileHeader />}</Head>
   );
 };
 
