3a6c704ad230adca3c34530adfc09f76391e464f
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-11-21 01:18:28 +0200

    btn promotion

--- src/modules/main/aboutProduct/sectionPromotion/SectionPromotion.jsx
+++ src/modules/main/aboutProduct/sectionPromotion/SectionPromotion.jsx
@@ -49,6 +49,7 @@ const SectionPromotion = ({ media }) => {
                     height={"15px"}
                     width={"49px"}
                     color={theme.color.darkGreen}
+                    margin={false}
                   >
                     <Span>Order now</Span>
                   </BtnGreen>
