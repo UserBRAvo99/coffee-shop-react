3a4b49faf13eea670916f26e0463bed1fef35249
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-11-22 16:34:02 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import "normalize.css";
-import "./App.css";
 import Header from "./modules/header/Header";
 import { Main } from "./modules/main/Main";
 import FooterShop from "./modules/footer/Footer";
 import Modal from "./modules/modal/Modal";
-// import { useContext } from "react";
-// import { ShopContext } from "./context/ShopProvider";
 
 function App() {
-  // const { isOpenModal } = useContext(ShopContext);
   return (
     <>
       <Header />
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { ShopProvider } from "./context/ShopProvider.jsx";
 import { Global } from "./Global.js";
 
